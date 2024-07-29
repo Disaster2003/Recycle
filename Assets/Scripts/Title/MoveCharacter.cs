e7dd54a864593f03170121012084bbbda73b6fda
Disaster <Disaster20031017@gmail.com>
2024-07-29 15:48:35 +0900

    Read .csv

--- Assets/Scripts/Title/MoveCharacter.cs
+++ Assets/Scripts/Title/MoveCharacter.cs
@@ -5,23 +5,24 @@ using System.IO;
 
 public class MoveCharacter : MonoBehaviour
 {
-    private TextAsset csvFile; // CSVファイル
-    private List<string[]> csvData = new List<string[]>(); // CSVファイルの中身を入れるリスト
-
     // 設定する位置
-    [SerializeField] Vector2 position_initialize, position_midpoint, position_goal;
+    private struct Position
+    {
+        public Vector2 initialize,
+            midpoint,
+            goal;
+    }
+    Position position;
 
-    [SerializeField] int SPEED_MOVE; // 移動速度
-
-    private float time_tmp;
-
-    [SerializeField] float time_wait;
+    private float time_tmp,
+        time_wait,
+        SPEED_MOVE;
 
     private enum JOB
     {
-        BOY,
-        GIRL,
-        MOM,
+        BOY = 1,
+        GIRL = 2,
+        MOM = 3,
     }
     [SerializeField] JOB job;
 
@@ -38,15 +39,30 @@ public class MoveCharacter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //csvFile = Resources.Load("TitleData") as TextAsset; // ResourcesにあるCSVファイルを格納
-        //StringReader reader = new StringReader(csvFile.text); // TextAssetをStringReaderに変換
-        //while (reader.Peek() != -1)
-        //{
-        //    string line = reader.ReadLine(); // 1行ずつ読み込む
-        //    csvData.Add(line.Split(',')); // csvDataリストに追加する
-        //}
+        TextAsset csvFile = Resources.Load("TitleData") as TextAsset; // ResourcesにあるCSVファイルを格納
+        StringReader reader = new StringReader(csvFile.text); // TextAssetをStringReaderに変換
+        List<string[]> csvData = new List<string[]>(); // CSVファイルの中身を入れるリスト
+        while (reader.Peek() != -1)
+        {
+            string line = reader.ReadLine(); // 1行ずつ読み込む
+            csvData.Add(line.Split(',')); // csvDataリストに追加する
+        }
+
+        int jobIndex = (int)job;
+        float initialize_x = float.Parse(csvData[jobIndex][1]);
+        float initialize_y = float.Parse(csvData[jobIndex][2]);
+        float midpoint_x = float.Parse(csvData[jobIndex][3]);
+        float midpoint_y = float.Parse(csvData[jobIndex][4]);
+        float goal_x = float.Parse(csvData[jobIndex][5]);
+        float goal_y = float.Parse(csvData[jobIndex][6]);
+        SPEED_MOVE = float.Parse(csvData[jobIndex][7]);
+        time_wait = float.Parse(csvData[jobIndex][8]);
+
+        position.initialize = new Vector2(initialize_x, initialize_y);
+        position.midpoint = new Vector2(midpoint_x, midpoint_y);
+        position.goal = new Vector2(goal_x, goal_y);
         // 位置の初期化
-        transform.localPosition = position_initialize;
+        transform.localPosition = position.initialize;
     }
 
     // Update is called once per frame
@@ -63,20 +79,20 @@ public class MoveCharacter : MonoBehaviour
             switch (arrived_position)
             {
                 case ARRIVED_POSITION.INITIALIZE:
-                    float tmp = (Time.time - time_tmp) * SPEED_MOVE / Vector2.Distance(position_initialize, position_midpoint);
-                    transform.localPosition = Vector2.Lerp(position_initialize, position_midpoint, tmp);
+                    float tmp = (Time.time - time_tmp) * SPEED_MOVE / Vector2.Distance(position.initialize, position.midpoint);
+                    transform.localPosition = Vector2.Lerp(position.initialize, position.midpoint, tmp);
 
-                    if (transform.localPosition.x == position_midpoint.x)
+                    if (transform.localPosition.x == position.midpoint.x)
                     {
                         arrived_position = ARRIVED_POSITION.MIDPOINT;
                         time_tmp = Time.time;
                     }
                     break;
                 case ARRIVED_POSITION.MIDPOINT:
-                    tmp = (Time.time - time_tmp) * SPEED_MOVE / Vector2.Distance(position_midpoint, position_goal);
-                    transform.localPosition = Vector2.Lerp(position_midpoint, position_goal, tmp);
+                    tmp = (Time.time - time_tmp) * SPEED_MOVE / Vector2.Distance(position.midpoint, position.goal);
+                    transform.localPosition = Vector2.Lerp(position.midpoint, position.goal, tmp);
 
-                    if (transform.localPosition.x == position_goal.x)
+                    if (transform.localPosition.x == position.goal.x)
                         arrived_position = ARRIVED_POSITION.GOAL;
                     break;
                 case ARRIVED_POSITION.GOAL:
